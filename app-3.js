fa2b760d6e1dbc9e3b20734a2f8b93e09d3b3029
Arsalan Sadri <asadri62@gmail.com>
2018-10-27 13:03:20 -0700

    adding app3

---
+++ app-3.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+app.use("/user/:id", (req, res, next) => {
+    console.log(`"Request method: ${req.method}"`);
+    next();
+}, (req, res, next) => {
+    console.log(`"Request URL: ${req.originalUrl}"`);
+    res.send(req.params.id);
+});
+
+app.use()
+
+const PORT = process.env.PORT || 8080;
+const host = "localhost";
+app.listen(PORT, host, () =>
+    console.log("App running on http://" + host + ":" + PORT));
